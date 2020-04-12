473d66f1803b0bd9b2016c9f27b7ee9899d00a5d
Amy <azli@andrew.cmu.edu>
2020-04-12 16:42:23 -0400

    Removed 'distance to populated hub' variable

--- LP_formulation.py
+++ LP_formulation.py
@@ -2,13 +2,12 @@ import pulp
 import xlrd
 
 class Beach:
-    def __init__(self, name, g, w, s, p, d):
+    def __init__(self, name, g, w, s, p):
         self.name = name
         self.g = g # grams of plastic pollution
         self.w = w # vulnerable marine wildlife population
         self.s = s # safety score and terrain
         self.p = p # population around area
-        self.d = d # distance from closest populated hub
 
     def get_name(self):
         return self.name
@@ -20,8 +19,6 @@ class Beach:
         return self.s
     def get_p(self):
         return self.p
-    def get_d(self):
-        return self.d
 
 # Global variables
 r = 50 # rate of cleanup (average grams of plastic picked up per hour, by one volunteer)
@@ -32,11 +29,11 @@ total_volunteers = 10 # total number of volunteers who have signed up
 # TODO: import data from excel sheet
 beach_dict = {}
 
-beach_0 = Beach("0", 230, 2.3, 5, 5, 1)
-beach_1 = Beach("1", 5699, 0.02, 4, 5, 1)
-beach_2 = Beach("2", 7800, 0.05, 3, 5, 1)
-beach_3 = Beach("3", 9289, 0.7, 4, 5, 1)
-beach_4 = Beach("4", 802, 0.6, 5, 5, 1)
+beach_0 = Beach("0", 230, 2.3, 5, 5)
+beach_1 = Beach("1", 5699, 0.02, 4, 5)
+beach_2 = Beach("2", 7800, 0.05, 3, 5)
+beach_3 = Beach("3", 9289, 0.7, 4, 5)
+beach_4 = Beach("4", 802, 0.6, 5, 5)
 
 beach_dict[0] = beach_0
 beach_dict[1] = beach_1
@@ -72,31 +69,17 @@ cleanup_hours = [2,2,2,2,2] # setting constant for now
 c1 = 3
 c2 = 1
 c3 = 0.5
-c4 = 0.3
-
-# There's a problem with the indicator variable in this part, so commenting out for now
-'''
-lp_problem += pulp.lpSum(
-    ((int(beach_volunteers[i]) > min_x) * # I_i
-        (c1*r*beach_volunteers[i]*cleanup_hours[i]*beach_dict[i].get_w() # c1*A_i*w_i where A_i = r*v_i*h_i
-        + c2*beach_dict[i].get_s() # c2*s_i
-        + c3*beach_dict[i].get_p() # c3*p_i
-        + c4*beach_dict[i].get_d() # c4*d_i
-        )) 
-    for i in range(num_beaches)), "Pollution Cleanup"
-'''
 
 lp_problem += pulp.lpSum(
-        (c1*r*beach_volunteers[i]*cleanup_hours[i]*beach_dict[i].get_w() # c1*A_i*w_i where A_i = r*v_i*h_i
+        (c1*r*beach_volunteers[i]*cleanup_hours[i]*beach_dict[i].get_w() # ci*r*v_i*h_i
         + c2*beach_dict[i].get_s() # c2*s_i
         + c3*beach_dict[i].get_p() # c3*p_i
-        + c4*beach_dict[i].get_d() # c4*d_i
         )
     for i in range(num_beaches)), "Pollution Cleanup"
 
 # Constraints
 for i in range(num_beaches):
-    lp_problem += r*beach_volunteers[i]*cleanup_hours[i] <= beach_dict[i].get_g() # A_i <= g_i
+    lp_problem += r*beach_volunteers[i]*cleanup_hours[i] <= beach_dict[i].get_g() # r*v_i*h_i <= g_i
     lp_problem += pulp.lpSum(beach_volunteers[i] for i in range(num_beaches)) <= total_volunteers
 
 print(lp_problem)
